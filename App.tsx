7abca066f814f97cda5389b6bcd2c04eeb020d52
Karl Beecken <karl@beecken.berlin>
2021-08-14 19:36:34 +0200

    🎉 init

--- App.tsx
+++ App.tsx
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import miner from "./miner";
+const version = "Alpha 0.1";
 
 export default function App() {
+  const [active, setActive] = useState(false);
+  const [buttonLabel, setButtonLabel] = useState("Daten sammeln starten");
+
+  useEffect(() => {
+    setButtonLabel(active ? "Daten sammeln stoppen" : "Daten sammeln starten");
+    console.log("🔨 rendered!");
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.head}>ICE Miner {version}</Text>
+      <Text style={styles.infoText}>
+        Zum Fortfahren müssen Sie sich im ICE-WLAN befinden.
+      </Text>
+      <Button
+        onPress={() => {
+          setActive(!active);
+          miner();
+        }}
+        title={buttonLabel}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +35,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    margin: 30,
+    justifyContent: "space-between",
+  },
+  head: {
+    fontWeight: "bold",
+    fontSize: 30,
+    textAlign: "center",
+    paddingBottom: 30,
+  },
+  infoText: {
+    fontStyle: "italic",
+    paddingBottom: 30,
   },
 });
---
+++ miner.ts
@@ -0,0 +1,5 @@
+import Ice from "icedata";
+export default async function () {
+  let res = await Ice.fetchAll();
+  //alert(res);
+}
